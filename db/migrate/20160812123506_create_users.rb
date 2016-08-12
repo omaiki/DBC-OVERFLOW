24864a0c89e379ccb96e833a72d0e6b8fab03c4c
Oshoke Maiki <oshokemaiki@gmail.com>
2016-08-12 12:44:13 -0700

    Create user models, migrations

---
+++ db/migrate/20160812123506_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :full_name
+      t.string :username
+      t.string :email
+      t.string :password
+
+    end
+  end
+end
---
+++ db/migrate/20160812123536_create_votes.rb
@@ -0,0 +1,7 @@
+class CreateVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+
+    end
+  end
+end
